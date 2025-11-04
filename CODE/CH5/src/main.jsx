1256c8be7bf522d887dd4163a44cece357715ba4
Brock <brockmhensley@gmail.com>
2025-11-04 00:16:42 -0500

    Add Bootstrap and protected login page with modern UI

--- CODE/CH5/src/main.jsx
+++ CODE/CH5/src/main.jsx
@@ -1,11 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {App} from './App.jsx'
-
-// Import Bootstrap FIRST
 import 'bootstrap/dist/css/bootstrap.min.css'
-
-// Then import your custom CSS
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
